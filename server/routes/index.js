74490dde1d44276805f98f21ac4200f5c3aae055
aco <wwsxuan@163.com>
2019-06-14 13:42:44 +0800

    跨域

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,11 @@ const router = express.Router();
 const {randomArr} = require('../util/util');
 const {getImages} = require('../util/qiniu');
 
+router.all('/static/*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 router.get('/authCode', (req, res) => {
   let date = new Date();
   date.setHours(4, 25, 0, 0);
