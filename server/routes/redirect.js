ec8577b65e0e6e62e472d34e6de59b4120b9bcb4
aco <wwsxuan@163.com>
2019-06-14 16:41:48 +0800

    redirect

--- server/routes/redirect.js
+++ server/routes/redirect.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/static/wallpaper4web/*', (req, res) => {
-  res.redirect('https://wallpaper.acohome.cn');
+router.get(/\/static\/wallpaper4web/, (req, res, next) => {
+  if (req.hostname !== 'wallpaper.acohome.cn') {
+    res.redirect('https://wallpaper.acohome.cn');
+  }
+  next();
 });
 
 module.exports = router;
