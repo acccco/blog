55bfa8b2708e7b73080b859e14512303d105f766
aco <wwsxuan@163.com>
2020-08-05 14:20:30 +0800

    提交消息头允许通过

--- server/routes/zebra-editor.js
+++ server/routes/zebra-editor.js
@@ -4,11 +4,11 @@ const { insert } = require("../util/mysql");
 
 router.all("*", (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "content-type");
   next();
 });
 
 router.post("/api/error-collection", async (req, res) => {
-  console.log(req.body);
   await insert("zebra", "error_collect", {
     error_stack: req.body.stack,
     error_message: req.body.errorMessage,
